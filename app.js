6b3c01e11831a4c6942abbe6bfd44c297b0cfb0d
James Flood Jr <floodadosious111@gmail.com>
2023-04-12 21:13:13 -0400

    new order

--- app.js
+++ app.js
@@ -2,12 +2,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const session = require('express-session')
+const albumArt = require('album-art')
 require('dotenv').config()
 const uri = process.env.MONGO_DB_CONNECTION_STRING
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const session = require('cookie-session')
-const albumArt = require('album-art')
-
 
 const app = express()
 app.use(cookieParser())
